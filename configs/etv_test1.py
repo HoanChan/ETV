a804e003153b61cd55217462893b4b763df5af37
Lê Hoàn Chân <le.hoan.chan@gmail.com>
2025-06-23 15:33:25 +0700

    Thêm chú thích và in config file để xem thử

--- configs/etv_test1.py
+++ configs/etv_test1.py
@@ -1,12 +1,13 @@
 _base_ = [
-    'mmocr::textrecog/_base_/default_runtime.py',
-    'mmocr::textrecog/_base_/schedules/schedule_adam_base.py',
-    'datasets/vitabset_data.py',
+    'mmocr::textrecog/_base_/default_runtime.py',               # mmocr:: tương đương với link tới folder /configs/ của mmocr
+    'mmocr::textrecog/_base_/schedules/schedule_adam_base.py',  # Xem thêm tại https://github.com/open-mmlab/mmocr/tree/v1.0.1/configs
+    'datasets/vitabset_data.py',                                # https://mmengine.readthedocs.io/en/latest/advanced_tutorials/config.html#inherit-configuration-files-across-repository
     '_etv_base.py'
 ]
 
 optim_wrapper = dict(optimizer=dict(lr=4e-4))
 train_cfg = dict(max_epochs=12)
+
 # learning policy
 param_scheduler = [
     dict(type='LinearLR', end=100, by_epoch=False),
@@ -14,6 +15,7 @@ param_scheduler = [
 ]
 
 train_dataset = _base_.vitabset_rec_test
+val_dataset = _base_.vitabset_rec_test
 test_dataset = _base_.vitabset_rec_test
 
 train_dataloader = dict(
@@ -29,9 +31,15 @@ val_dataloader = dict(
     persistent_workers=True,
     drop_last=False,
     sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=val_dataset)
+
+test_dataloader = dict(
+    batch_size=2,
+    num_workers=1,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
     dataset=test_dataset)
 
-test_dataloader = val_dataloader
-
 val_evaluator = dict(dataset_prefixes=['Toy'])
-test_evaluator = val_evaluator
+test_evaluator = dict(dataset_prefixes=['Toy'])
