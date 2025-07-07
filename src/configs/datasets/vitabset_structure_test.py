798e10f95fbff40e7ee7dbd16e97f0e7bc27a8b3
Lê Hoàn Chân <le.hoan.chan@gmail.com>
2025-07-07 16:20:50 +0700

    Sửa vitabset_structure_test.py cho giống với template

--- src/configs/datasets/vitabset_structure_test.py
+++ src/configs/datasets/vitabset_structure_test.py
@@ -1,17 +1,5 @@
 from config import *
 
-test_dataset = {
-    'ann_file': VITABSET_TEST_JSON,
-    'data_prefix': {'img_path': VITABSET_TEST_IMAGE_ROOT},
-    'task_type': 'both',
-    'split_filter': None,  # Load all splits available in the file
-    'max_structure_len': 500,
-    'max_cell_len': 500,
-    'ignore_empty_cells': True
-}
-TRAIN_STATE = False  # Set to False for test mode
-img_norm_cfg = dict(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
-
 test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
@@ -24,15 +12,32 @@ test_pipeline = [
         pad_val=0,
         return_mask=True,
         mask_ratio=(8, 8),
-        train_state=TRAIN_STATE),
-    #dict(type='TableBboxEncode'),
+        train_state=False),
+    dict(type='TableBboxEncode'),
     dict(type='ToTensorOCR'),
-    dict(type='NormalizeOCR', **img_norm_cfg),
+    dict(type='NormalizeOCR', mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
     dict(
         type='Collect',
         keys=['img'],
         meta_keys=[ 'filename', 'ori_shape', 'img_shape', 'scale_factor', 'img_norm_cfg', 'ori_filename', 'pad_shape' ]),
 ]
+
+test_dataset = dict(
+    type='PubTabNetDataset',
+    ann_file=VITABSET_TEST_JSON,
+    data_prefix={'img_path': VITABSET_TEST_IMAGE_ROOT},
+    task_type='both',
+    split_filter=None,  # Load all splits available in the file
+    max_structure_len=500,
+    max_cell_len=500,
+    ignore_empty_cells=True,
+    max_data=-1,  # -1 để load toàn bộ, >0 để giới hạn số lượng sample
+    random_sample=False, 
+    pipeline=test_pipeline
+)
+
+TRAIN_STATE = False  # Set to False for test mode
+img_norm_cfg = dict(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
 
 test_dataloader = dict(
     batch_size=2,
