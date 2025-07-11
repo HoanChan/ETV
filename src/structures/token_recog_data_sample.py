755a0864cbbd4e4a4741c59c622ed6260a9edd23
Lê Hoàn Chân <le.hoan.chan@gmail.com>
2025-07-11 09:45:17 +0700

    Cập nhật TokenRecogDataSample: cải thiện tài liệu và thêm các thuộc tính cho gt_tokens, pred_tokens, gt_bboxs, và pred_bboxs

--- src/structures/token_recog_data_sample.py
+++ src/structures/token_recog_data_sample.py
@@ -2,80 +2,106 @@
 from mmengine.structures import BaseDataElement, LabelData
 
 class TokenRecogDataSample(BaseDataElement):
-    """A data structure interface of MMOCR for token recognition. They are used
-    as interfaces between different components.
+    """A data structure interface of MMOCR for token recognition.
 
-    The attributes in ``TokenRecogDataSample`` are divided into two parts:
-
-        - ``gt_token``(LabelData): Ground truth token.
-        - ``pred_token``(LabelData): predictions token.
+    Attributes:
+        gt_tokens (LabelData): Ground truth tokens.
+        pred_tokens (LabelData): Predicted tokens.
+        gt_bboxs (LabelData): Ground truth bounding boxes.
+        pred_bboxs (LabelData): Predicted bounding boxes.
 
     Examples:
-         >>> import torch
-         >>> import numpy as np
-         >>> from mmengine.structures import LabelData
-         >>> from mmocr.data import TokenRecogDataSample
-         >>> # gt_token
-         >>> data_sample = TokenRecogDataSample()
-         >>> img_meta = dict(img_shape=(800, 1196, 3),
-         ...                 pad_shape=(800, 1216, 3))
-         >>> gt_token = LabelData(metainfo=img_meta)
-         >>> gt_token.item = 'mmocr'
-         >>> data_sample.gt_token = gt_token
-         >>> assert 'img_shape' in data_sample.gt_token.metainfo_keys()
-         >>> print(data_sample)
-         <TokenRecogDataSample(
-             META INFORMATION
-             DATA FIELDS
-             gt_token: <LabelData(
-                     META INFORMATION
-                     pad_shape: (800, 1216, 3)
-                     img_shape: (800, 1196, 3)
-                     DATA FIELDS
-                     item: 'mmocr'
-                 ) at 0x7f21fb1b9190>
-         ) at 0x7f21fb1b9880>
-         >>> # pred_token
-         >>> pred_token = LabelData(metainfo=img_meta)
-         >>> pred_token.item = 'mmocr'
-         >>> data_sample = TokenRecogDataSample(pred_token=pred_token)
-         >>> assert 'pred_token' in data_sample
-         >>> data_sample = TokenRecogDataSample()
-         >>> gt_token_data = dict(item='mmocr')
-         >>> gt_token = LabelData(**gt_token_data)
-         >>> data_sample.gt_token = gt_token
-         >>> assert 'gt_token' in data_sample
-         >>> assert 'item' in data_sample.gt_token
+        >>> import numpy as np
+        >>> from mmengine.structures import LabelData
+        >>> from mmocr.data import TokenRecogDataSample
+        >>> data_sample = TokenRecogDataSample()
+        >>> data_sample.gt_tokens = LabelData(item=['token1', 'token2'])
+        >>> data_sample.pred_tokens = LabelData(item=['token1', 'tokenX'])
+        >>> data_sample.gt_bboxs = LabelData(item=np.array([[0, 0, 10, 10]]))
+        >>> data_sample.pred_bboxs = LabelData(item=np.array([[1, 1, 11, 11]]))
+        >>> print(data_sample)
+        <TokenRecogDataSample(
+            META INFORMATION
+            DATA FIELDS
+            gt_tokens: <LabelData(
+                    META INFORMATION
+                    DATA FIELDS
+                    item: ['token1', 'token2']
+                ) at ...>
+            pred_tokens: <LabelData(
+                    META INFORMATION
+                    DATA FIELDS
+                    item: ['token1', 'tokenX']
+                ) at ...>
+            gt_bboxs: <LabelData(
+                    META INFORMATION
+                    DATA FIELDS
+                    item: [[ 0  0 10 10]]
+                ) at ...>
+            pred_bboxs: <LabelData(
+                    META INFORMATION
+                    DATA FIELDS
+                    item: [[ 1  1 11 11]]
+                ) at ...>
+        ) at ...>
     """
 
     @property
-    def gt_token(self) -> LabelData:
-        """LabelData: ground truth token.
-        """
-        return self._gt_token
+    def gt_tokens(self) -> LabelData:
+        """LabelData: ground truth tokens."""
+        return self._gt_tokens
 
-    @gt_token.setter
-    def gt_token(self, value: LabelData) -> None:
-        """gt_token setter."""
-        self.set_field(value, '_gt_token', dtype=LabelData)
+    @gt_tokens.setter
+    def gt_tokens(self, value: LabelData) -> None:
+        """gt_tokens setter."""
+        self.set_field(value, '_gt_tokens', dtype=LabelData)
 
-    @gt_token.deleter
-    def gt_token(self) -> None:
-        """gt_token deleter."""
-        del self._gt_token
+    @gt_tokens.deleter
+    def gt_tokens(self) -> None:
+        """gt_tokens deleter."""
+        del self._gt_tokens
 
     @property
-    def pred_token(self) -> LabelData:
-        """LabelData: prediction token.
-        """
-        return self._pred_token
+    def pred_tokens(self) -> LabelData:
+        """LabelData: predicted tokens."""
+        return self._pred_tokens
 
-    @pred_token.setter
-    def pred_token(self, value: LabelData) -> None:
-        """pred_token setter."""
-        self.set_field(value, '_pred_token', dtype=LabelData)
+    @pred_tokens.setter
+    def pred_tokens(self, value: LabelData) -> None:
+        """pred_tokens setter."""
+        self.set_field(value, '_pred_tokens', dtype=LabelData)
 
-    @pred_token.deleter
-    def pred_token(self) -> None:
-        """pred_token deleter."""
-        del self._pred_token
+    @pred_tokens.deleter
+    def pred_tokens(self) -> None:
+        """pred_tokens deleter."""
+        del self._pred_tokens
+
+    @property
+    def gt_bboxs(self) -> LabelData:
+        """LabelData: ground truth bounding boxes."""
+        return self._gt_bboxs
+
+    @gt_bboxs.setter
+    def gt_bboxs(self, value: LabelData) -> None:
+        """gt_bboxs setter."""
+        self.set_field(value, '_gt_bboxs', dtype=LabelData)
+
+    @gt_bboxs.deleter
+    def gt_bboxs(self) -> None:
+        """gt_bboxs deleter."""
+        del self._gt_bboxs
+
+    @property
+    def pred_bboxs(self) -> LabelData:
+        """LabelData: predicted bounding boxes."""
+        return self._pred_bboxs
+
+    @pred_bboxs.setter
+    def pred_bboxs(self, value: LabelData) -> None:
+        """pred_bboxs setter."""
+        self.set_field(value, '_pred_bboxs', dtype=LabelData)
+
+    @pred_bboxs.deleter
+    def pred_bboxs(self) -> None:
+        """pred_bboxs deleter."""
+        del self._pred_bboxs
